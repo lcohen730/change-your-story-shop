7db0234ca8df0b6b17d7af9ba703c358ea371983
lcohen730 <lcohen730@gmail.com>
2023-10-20 16:35:31 -0400

    Minor Utilities Updates

--- src/utilities/orders-api.js
+++ src/utilities/orders-api.js
@@ -7,7 +7,6 @@ export function getCart() {
     return sendRequest(`${BASE_URL}/cart`);
 }
 
-// Add an item to the cart
 export function addItemToCart(itemId) {
     // Just send itemId for best security (no pricing)
     return sendRequest(`${BASE_URL}/cart/items/${itemId}`, 'POST');
