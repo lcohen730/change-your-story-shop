220a0430e413cfcd2be49d41a10b710318e70ef8
lcohen730 <lcohen730@gmail.com>
2023-11-05 16:57:58 -0500

    Add New Pictures and Update Seed File

--- config/seed.js
+++ config/seed.js
@@ -8,24 +8,31 @@ const Item = require('../models/item');
     await Item.deleteMany({});
     const items = await Item.create([
         {   
-            name: 'T-Shirt',
-            img: "https://i.imgur.com/1QQlGnz.png",
+            name: 'Change Your Story T-Shirt',
+            /* img: "https://i.imgur.com/1QQlGnz.png", */
+            img: "https://i.imgur.com/LsGJIX5.jpg",
             price: 29.99 
         },
         {   
-            name: 'Canvas Bag',
+            name: 'Canvas Bag with Logo',
             img: "https://i.imgur.com/e03G811.png",
             price: 24.99 
         },
         {   
-            name: 'Bracelet',
-            img: "https://i.imgur.com/I1A8fDg.png",
+            name: 'Charm Bracelet with Flower',
+            /* img: "https://i.imgur.com/I1A8fDg.png", */
+            img: "https://i.imgur.com/DxYYPrQ.jpg",
             price: 14.99 
         },
         {   
             name: 'Mug',
             img: "https://i.imgur.com/I1A8fDg.png",
             price: 29.99 
+        },
+        {   
+            name: 'Charm Bracelet with Inspirational Message',
+            img: "https://i.imgur.com/b684K1v.jpg",
+            price: 14.99 
         },
     ]);
   
